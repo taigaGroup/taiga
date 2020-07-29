bc266c6f6f5d4202f4f72c7d9510082e6b3d0546
satyshev <satyshev@jinr.ru>
2020-07-29 15:49:15 +0300

    Add testCamera macro.

---
+++ macro/test/testCamera.C
@@ -0,0 +1,19 @@
+void testCamera()
+{
+  TAIGACamera camera;
+	Int_t nnei=1407;
+	Double_t xc(0.), yc(0.);
+  for (Int_t ii = 0; ii < NUMBER_OF_CLUSTERS; ii++) {
+  	for (Int_t jj = 0; jj < NUMBER_OF_PIXELS; jj++) {
+  		camera.SetPixelXcor(xc, ii, jj);
+  		camera.SetPixelYcor(yc, ii, jj);
+  		camera.SetPixelAmp(xc, ii, jj);
+  		camera.SetIsPixelTriggered(kTRUE, ii, jj);
+  		for (Int_t kk = 0; kk < nnei; kk++) {
+        camera.AddPixelNeighbor(ii, 0, jj, 0);
+  		}
+  		xc+=1.; yc-=1.; nnei--;
+  	}
+  }
+  camera.Print();
+}
